142ddc832371e037135b0a7cc960839135fa103c
fillipe.felix <felipesoares_1993@hotmail.com>
2022-02-15 15:14:46 -0300

    Criando classe de configuração do Identity e o LoginDbContext

---
+++ Project_Login/Configuration/IdentityConfig.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+using Project_Login.Extensions;
+using Project_Login.Persistence;
+
+namespace Project_Login.Configuration;
+
+public static class IdentityConfig
+{
+    public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services,
+                                                              IConfiguration configuration)
+    {
+        services.AddDbContext<LoginDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddIdentityCore<IdentityUser>()
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<LoginDbContext>()
+            .AddErrorDescriber<IdentityMensagensPortugues>()
+            .AddDefaultTokenProviders();
+        
+        // JWT
+
+        var appSettingsSection = configuration.GetSection("AppSettings");
+        services.Configure<AppSettings>(appSettingsSection);
+
+        var appSettings = appSettingsSection.Get<AppSettings>();
+        var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(x =>
+        {
+            x.RequireHttpsMetadata = false;
+            x.SaveToken = true;
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = appSettings.ValidoEm,
+                ValidIssuer = appSettings.Emissor
+            };
+        });
+        
+        return services;
+    }
+}
---
+++ Project_Login/Persistence/LoginDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project_Login.Persistence;
+
+public class LoginDbContext : IdentityDbContext
+{
+    public LoginDbContext(DbContextOptions<LoginDbContext> options) : base(options)
+    {
+        
+    }
+}
