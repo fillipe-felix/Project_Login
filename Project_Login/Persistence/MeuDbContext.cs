aa85d4338f2fe29dcd6d58127927fa489434810c
fillipe.felix <felipesoares_1993@hotmail.com>
2022-02-16 11:04:29 -0300

    Criando as migrations de Produtos

---
+++ Project_Login/Persistence/MeuDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+using Microsoft.EntityFrameworkCore;
+
+using Project_Login.Models;
+
+namespace Project_Login.Persistence;
+
+public class MeuDbContext : DbContext
+{
+    
+    public MeuDbContext(DbContextOptions<MeuDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Produto> Produtos { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
