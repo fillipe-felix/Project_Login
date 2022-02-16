cd051d184b7f68c0d14b088db885329245d7df2f
fillipe.felix <felipesoares_1993@hotmail.com>
2022-02-16 11:59:42 -0300

    Adicionando a controller de produto

---
+++ Project_Login/Configuration/ApiConfig.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Login.Configuration;
+
+public static class ApiConfig
+{
+    public static IServiceCollection WebApiConfig(this IServiceCollection services)
+    {
+        services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.SuppressModelStateInvalidFilter = true;
+        });
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("Development", builder => builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            //.AllowCredentials());
+        });
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseMvcConfiguration(this IApplicationBuilder app)
+    {
+        app.UseHttpsRedirection();
+        app.UseCors("Development");
+        //app.UseMvc();
+
+        return app;
+    }
+}
---
+++ Project_Login/Controllers/ProdutoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using Project_Login.Extensions;
+using Project_Login.Models;
+using Project_Login.Persistence;
+
+namespace Project_Login.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+public class ProdutoController : MainController
+{
+    private readonly MeuDbContext _context;
+
+    public ProdutoController(MeuDbContext context)
+    {
+        _context = context;
+    }
+
+    [ClaimsAuthorize("Produto", "Cadastrar")]
+    [HttpPost]
+    public async Task<IActionResult> Cadastrar([FromBody] Produto produto)
+    {
+        await _context.Produtos.AddAsync(produto);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+    
+    
+    [ClaimsAuthorize("Produto", "Listar")]
+    [HttpGet()]
+    public async Task<IActionResult> Listar()
+    {
+        var listProduto = await _context.Produtos.ToListAsync();
+        
+        return Ok(listProduto);
+    }
+}
