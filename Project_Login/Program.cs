84d71fafa77178ca749975871282bc789b567459
fillipe.felix <felipesoares_1993@hotmail.com>
2022-02-16 16:04:07 -0300

    Adicionando authenticação pelo swagger

--- Project_Login/Program.cs
+++ Project_Login/Program.cs
@@ -14,7 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerConfig();
 
 builder.Services.AddDbContext<MeuDbContext>(options =>
 {
