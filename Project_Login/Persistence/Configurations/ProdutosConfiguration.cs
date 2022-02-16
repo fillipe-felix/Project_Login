6e70dbd2b37278944006ac58baae0b65cc00fd70
fillipe.felix <felipesoares_1993@hotmail.com>
2022-02-16 11:09:32 -0300

    Adicionando as configurações do FluentValidation para Produto

---
+++ Project_Login/Persistence/Configurations/ProdutosConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using Project_Login.Models;
+
+namespace Project_Login.Persistence.Configurations;
+
+public class sConfiguration : IEntityTypeConfiguration<Produto>
+{
+
+    public void Configure(EntityTypeBuilder<Produto> builder)
+    {
+        builder.HasKey(s => s.Id);
+    }
+}
